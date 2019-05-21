1f048a1e1d32f03a8776c02b114162160bfb2fc8
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-21 14:54:08 -0300

    :sparkles: Calculando a área e o perímetro do círculo

--- lib/circle.rb
+++ lib/circle.rb
@@ -4,11 +4,16 @@ class Circle < GeometricBase
   attr_writer :radius
 
   def initialize(radius)
+    @radius = radius
   end
 
   def area
+    sqrRadius = exponentiation(@radius, 2)
+    multiply(PI, sqrRadius)
   end
 
   def perimeter
+    doublePi = multiply(2, PI)
+    multiply(doublePi, @radius)
   end
 end
