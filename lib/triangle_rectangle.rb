1fcd5f61a65d40ae31aa2284b76e7e566f7196f2
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-21 15:26:56 -0300

    :sparkles: Calculando a área e o perímetro do triângulo retângulo

--- lib/triangle_rectangle.rb
+++ lib/triangle_rectangle.rb
@@ -1,16 +1,32 @@
 require_relative "geometric_base"
+require_relative "rectangle"
+
 
 class TriangleRectangle < GeometricBase
   attr_writer :height, :base
 
   def initialize(base, height)
+    @height = height
+    @base = base
   end
 
   def area
-    0
+    @rectangle = Rectangle.new(@height, @base)
+    divide(@rectangle.area, 2)
   end
 
   def perimeter
-    0
+    squareBase = exponentiation(@base, 2)
+    squareHeight = exponentiation(@height, 2)
+    sqrBasePlusSqrHeight = sum(squareBase, squareHeight)
+    basePlusHeight = sum(@base, @height)
+
+    sum(
+      radiciation(
+        sqrBasePlusSqrHeight,
+        2
+      ),
+      basePlusHeight
+    )
   end
 end
