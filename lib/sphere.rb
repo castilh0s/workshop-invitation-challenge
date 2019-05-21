7a5f4dbad23f5b574274be48a5aa2e4adf33623c
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-21 15:13:04 -0300

    :sparkles: Calculando área e volume da esfera

--- lib/sphere.rb
+++ lib/sphere.rb
@@ -1,11 +1,16 @@
 require_relative "geometric_base"
 
 class Sphere < GeometricBase
+  attr_writer :radius
+
   def initialize(radius)
+    @radius = radius
   end
 
   def area
-    0
+    quadruplePi = multiply(4, PI)
+    squareRadius = exponentiation(@radius, 2)
+    multiply(quadruplePi, squareRadius)
   end
 
   def perimeter
@@ -13,6 +18,9 @@ class Sphere < GeometricBase
   end
 
   def volume
-    0
+    cubeRadius = exponentiation(@radius, 3)
+    fourThirds = divide(4,3)
+    piCubeRadius = multiply(PI, cubeRadius)
+    multiply(fourThirds, piCubeRadius)
   end
 end
